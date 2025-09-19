2aef493bbe25d8c1d272c69a5e1adb6852c029ce
gabrdsp <gabrdsp@outlook.com>
2025-09-19 14:34:01 -0300

    Adicionando a questão 2 do nível 4.

--- Lista2/Nivel4/Main.java
+++ Lista2/Nivel4/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Questao1.run();
+    
+        Questao2.run();
     }
 }
---
+++ Lista2/Nivel4/Questao2.java
@@ -0,0 +1,26 @@
+// Questao2.java
+import java.util.Scanner;
+
+public class Questao2 {
+
+    public static void run() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de números a processar: ");
+        int qtd = sc.nextInt();
+
+        for (int i = 1; i <= qtd; i++) {
+            System.out.print("Digite o " + i + "º número: ");
+            int n = sc.nextInt();
+
+            long fatorial = 1;
+            for (int j = 1; j <= n; j++) {
+                fatorial *= j;
+            }
+
+            System.out.println("Fatorial de " + n + " = " + fatorial);
+        }
+
+        sc.close();
+    }
+}
