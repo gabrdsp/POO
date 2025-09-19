caf8d225d92873a80f932b8b75cea505ed887697
gabrdsp <gabrdsp@outlook.com>
2025-09-19 14:52:16 -0300

    Adicionando a questão 4 do nível 4.

--- Lista2/Nivel4/Main.java
+++ Lista2/Nivel4/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
     
-        Questao03.run();
+        Questao04.run();
     }
 }
---
+++ Lista2/Nivel4/Questao04.java
@@ -0,0 +1,11 @@
+// Questao4.java
+public class Questao04 {
+
+    public static void run() {
+        System.out.println("Números de 1 até 2000:");
+        for (int i = 1; i <= 2000; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
